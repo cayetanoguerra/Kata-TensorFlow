ab94866e737088c94ba776f2a0a0d2a2ef54490c
Cayetano <cayetanoguerra@gmail.com>
2016-02-04 15:40:13 +0000

    Kata

---
+++ iris.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Translate a list of labels into an array of 0's and one 1.
+# i.e.: 4 -> [0,0,0,0,1,0,0,0,0,0] 
+def one_hot(x, n):
+    if type(x) == list:
+        x = np.array(x)
+    x = x.flatten()
+    o_h = np.zeros((len(x), n))
+    o_h[np.arange(len(x)), x] = 1
+    return o_h
+
+
+data = np.genfromtxt('iris.data', delimiter=",")
+np.random.shuffle(data)
+x_data = data[:,0:4].astype('f4')
+y_data = one_hot(data[:,4].astype(int), 3)
+
+print y_data
+
+print "\nSome samples..."
+for i in range(20):
+    print x_data[i], " -> ", y_data[i]
+print
+
+x = tf.placeholder("float", [None, 4])
+y_ = tf.placeholder("float", [None, 3])
+
+
+W_hidden = tf.Variable(np.float32(np.random.rand(4, 5))*0.01)
+b_hidden = tf.Variable(np.float32(np.random.rand(5))*0.01)
+
+W_output = tf.Variable(np.float32(np.random.rand(5, 3))*0.01)
+b_output = tf.Variable(np.float32(np.random.rand(3))*0.01)
+
+y_hidden = tf.sigmoid(tf.matmul(x, W_hidden) + b_hidden)
+#y = tf.nn.softmax(tf.sigmoid(tf.matmul(y_hidden, W_output) + b_output))
+y = tf.nn.softmax(tf.matmul(y_hidden, W_output) + b_output)
+
+
+cross_entropy = tf.reduce_sum(tf.square(y_ - y))
+#cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+
+#train = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+train = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)
+
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
+
+print "----------------------"
+print "   Start training...  "
+print "----------------------"
+
+batch_size = 10
+
+for step in xrange(10000):
+    for jj in xrange(len(x_data) / batch_size):
+        batch_xs = x_data[jj*batch_size : jj*batch_size+batch_size]
+        batch_ys = y_data[jj*batch_size : jj*batch_size+batch_size]
+        #print batch_xs, batch_ys
+        sess.run(train, feed_dict={x: batch_xs, y_: batch_ys})
+        if step % 500 == 0:
+            print "Iteration #:", step, "Error: ", sess.run(cross_entropy, feed_dict={x: batch_xs, y_: batch_ys})
+            print sess.run(y, feed_dict={x: batch_xs})
+            print batch_ys
+            print "----------------------------------------------------------------------------------"
